6a04664d71285d05fa07488943dcc95c8dbe96a2
Ming Huang <mingh@nvidia.com>
2021-01-12 16:58:52 +0800

    Added fc_sparse layer

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -368,6 +368,63 @@ def fc(input,
     # add activation
     return helper.append_activation(pre_activation)
 
+def fc_sparse(input,
+       size,
+       num_flatten_dims=1,
+       param_attr=None,
+       bias_attr=None,
+       act=None,
+       name=None):
+    helper = LayerHelper("fc_sparse", **locals())
+    check_type(input, 'input', (list, tuple, Variable), 'fc_sparse')
+    if isinstance(input, (list, tuple)):
+        for i, input_x in enumerate(input):
+            check_type(input_x, 'input[' + str(i) + ']', Variable, 'fc_sparse')
+    dtype = helper.input_dtype()
+    check_dtype(dtype, 'input', ['float16', 'float32', 'float64'], 'fc_sparse')
+    if convert_dtype(dtype) in ['float32', 'float64']:
+        fc(input, size, num_flatten_dims, param_attr, bias_attr, act, name)
+        warnings.warn("fc_sparse only support float16 on GPUs. Insert dense fc.")
+    else:
+        mul_results = []
+        for input_var, param_attr in helper.iter_inputs_and_params():
+            input_shape = input_var.shape
+            if num_flatten_dims == -1:
+                num_flatten_dims = len(input_shape) - 1
+            param_shape = [
+                reduce(lambda a, b: a * b, input_shape[num_flatten_dims:], 1)
+            ] + [size]
+
+            w = helper.create_parameter(
+                attr=param_attr, shape=param_shape, dtype=dtype, is_bias=False)
+            tmp = helper.create_variable_for_type_inference(dtype)
+
+            input_transposed = transpose(input_var, perm=[1, 0])
+            w_transposed = transpose(w, perm=[1, 0])
+
+            helper.append_op(
+                type="mul",
+                inputs={"X": w_transposed,
+                        "Y": input_transposed},
+                outputs={"Out": tmp},
+                attrs={"x_num_col_dims": 1,
+                    "y_num_col_dims": num_flatten_dims})
+            tmp = transpose(tmp, perm=[1, 0])
+            mul_results.append(tmp)
+
+        if len(mul_results) == 1:
+            pre_bias = mul_results[0]
+        else:
+            pre_bias = helper.create_variable_for_type_inference(dtype)
+            helper.append_op(
+                type="sum",
+                inputs={"X": mul_results},
+                outputs={"Out": pre_bias},
+                attrs={"use_mkldnn": False})
+        # add bias
+        pre_activation = helper.append_bias_op(pre_bias, dim_start=num_flatten_dims)
+        # add activation
+        return helper.append_activation(pre_activation)
 
 @deprecated(since="2.0.0", update_to="paddle.nn.functional.embedding")
 def embedding(input,
