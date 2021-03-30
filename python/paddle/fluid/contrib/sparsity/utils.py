21fe79c8c0c21069e684e066af4e9a5e083cb786
Ming Huang <mingh@nvidia.com>
2021-03-30 17:44:17 +0800

    Added a draf of doc in sparsity/utils.py

--- python/paddle/fluid/contrib/sparsity/utils.py
+++ python/paddle/fluid/contrib/sparsity/utils.py
@@ -1,3 +1,20 @@
+# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Utilities of Auto SParsity (ASP).
+"""
+
 from __future__ import print_function
 
 import sys
@@ -6,24 +23,61 @@ import collections
 import numpy as np
 from itertools import permutations
 
-__all__ = ['density', 'reshape_1d', 'check_mask_1d', 'get_mask_1d_greedy',
-           'check_mask_2d', 'get_mask_2d_greedy', 'create_mask', 'check_sparsity']
+__all__ = [
+    'density', 'reshape_1d', 'check_mask_1d', 'get_mask_1d_greedy',
+    'get_mask_1d_best',
+    'check_mask_2d', 'get_mask_2d_greedy', 'get_mask_2d_best', 'create_mask',
+    'check_sparsity'
+]
+
 
 def density(x):
+    """
+    Calculate density of input matrix.
+
+    Args:
+        x (nparray): The input matrix.
+    Returns:
+        float: The density of x.
+    """
     x_flattened = x.flatten()
     return float(np.nonzero(x_flattened)[0].size) / x_flattened.size
 
+
 def reshape_1d(mat, m):
+    """
+    Reshape input matrix to shape (-1, m)
+    If the second dimension of mat is not a multiples of m,
+    then this function would pad the remainder with 0 before reshaping.
+
+    Args:
+        mat (nparray): The input matrix.
+        m (int): The second dimension of reshaped matrix.
+    Returns:
+        tuple: (The reshaped mat, The shape of padded mat).
+    """
     remainder = mat.shape[1] % m
     if mat.shape[1] % m > 0:
-        mat_padded = np.zeros((mat.shape[0], mat.shape[1]+(m-remainder)))
+        mat_padded = np.zeros((mat.shape[0], mat.shape[1] + (m - remainder)))
         mat_padded[:, :mat.shape[1]] = mat
         shape = mat_padded.shape
         return mat_padded.reshape(-1, m), shape
     else:
         return mat.reshape(-1, m), mat.shape
 
+
 def check_mask_1d(mat, m, n):
+    """
+    Check if the input matrix is in 1D n:m sparse pattern.
+    1D n:m sparse pattern: There are must n zeros in every 1xm block.
+
+    Args:
+        mat (nparray): The input matrix.
+        m (int): m of n:m sparse pattern.
+        n (int): n of n:m sparse pattern.
+    Returns:
+        bool: True if mat in 1D n:m sparse pattern, otherwise False.
+    """
     if len(mat.shape) <= 1:
         mat_flattern, shape = reshape_1d(mat.reshape(1, mat.shape[0]), m)
     else:
@@ -34,7 +88,18 @@ def check_mask_1d(mat, m, n):
             return False
     return True
 
+
 def get_mask_1d_greedy(mat, m, n):
+    """
+    Compute 1D n:m sparse pattern mask for the input matrix mat in greedy way.
+
+    Args:
+        mat (nparray): The input matrix.
+        m (int): m of n:m sparse pattern.
+        n (int): n of n:m sparse pattern.
+    Returns:
+        nparray: The 1D n:m sparse mask of mat.
+    """
     mat_flattern, shape = reshape_1d(mat, m)
 
     mask_flattern = np.ones_like(mat_flattern)
@@ -47,8 +112,18 @@ def get_mask_1d_greedy(mat, m, n):
     mask[:, :] = mask_flattern[:, :mat.shape[1]]
     return mask
 
+
 valid_1d_patterns = {}
 def compute_valid_1d_patterns(m, n):
+    """
+    Compute all vaild 1D n:m sparse pattern.
+
+    Args:
+        m (int): m of n:m sparse pattern.
+        n (int): n of n:m sparse pattern.
+    Returns:
+        map: A map from m_n (string) to vaild 1D n:m sparse patterns.
+    """
     global valid_1d_patterns
 
     valid_key = '{}_{}'.format(m, n)
@@ -61,7 +136,18 @@ def compute_valid_1d_patterns(m, n):
         valid_1d_patterns[valid_key] = valid_patterns
         return valid_patterns
 
+
 def get_mask_1d_best(mat, m, n):
+    """
+    Compute 1D n:m sparse pattern mask for the input matrix mat in best way.
+
+    Args:
+        mat (nparray): The input matrix.
+        m (int): m of n:m sparse pattern.
+        n (int): n of n:m sparse pattern.
+    Returns:
+        nparray: The 1D n:m sparse mask of mat.
+    """
     patterns = compute_valid_1d_patterns(m, n)
 
     mat_flattern, shape = reshape_1d(mat, m)
@@ -71,27 +157,56 @@ def get_mask_1d_best(mat, m, n):
     mask = mask_flattern.reshape(mat.shape)
     return mask
 
+
 def reshape_2d(mat, m):
+    """
+    Reshape input matrix to shape (-1, m*m)
+    If the first and second dimension of mat is not a multiples of m,
+    then this function would pad the remainders with 0 before reshaping.
+
+    Args:
+        mat (nparray): The input matrix.
+        m (int): The second dimension of reshaped matrix.
+    Returns:
+        tuple: (The reshaped mat, The shape of padded mat).
+    """
     remainder_0 = mat.shape[0] % m
     remainder_1 = mat.shape[1] % m
 
-    new_shape = (mat.shape[0] if remainder_0 == 0 else mat.shape[0]+(m-remainder_0),
-                 mat.shape[1] if remainder_1 == 0 else mat.shape[1]+(m-remainder_1))
+    new_shape = (mat.shape[0] if remainder_0 == 0 \
+                 else mat.shape[0] + (m - remainder_0),
+                 mat.shape[1] if remainder_1 == 0 \
+                 else mat.shape[1] + (m - remainder_1))
     mat_padded = np.zeros(new_shape)
     mat_padded[:mat.shape[0], :mat.shape[1]] = mat
 
-    mat_flattern = np.empty(new_shape).reshape(-1, m*m)
+    mat_flattern = np.empty(new_shape).reshape(-1, m * m)
     curr_idx = 0
     for row_start in range(0, mat_padded.shape[0], m):
         row_end = row_start + m
         for col_start in range(0, mat_padded.shape[1], m):
             col_end = col_start + m
-            sub_mat = np.squeeze(mat_padded[row_start:row_end, col_start:col_end].reshape(-1))
+            sub_mat = np.squeeze(mat_padded[row_start:row_end, \
+                                            col_start:col_end] \
+                                            .reshape(-1))
             mat_flattern[curr_idx] = sub_mat
             curr_idx += 1
     return mat_flattern, mat_padded.shape
 
+
 def check_mask_2d(mat, m, n):
+    """
+    Check if the input matrix is in 2D n:m sparse pattern.
+    2D n:m sparse pattern: There are must n*n zeros in every mxm block,
+    under the constraint of selecting exactly n elements for each row and column.
+
+    Args:
+        mat (nparray): The input matrix.
+        m (int): m of n:m sparse pattern.
+        n (int): n of n:m sparse pattern.
+    Returns:
+        bool: True if mat in 2D n:m sparse pattern, otherwise False.
+    """
     mat_padded, shape = reshape_2d(mat, m)
     for sub_mat in mat_padded:
         sub_mask = np.absolute(np.squeeze(sub_mat.reshape(m, m))) > 0
@@ -100,7 +215,18 @@ def check_mask_2d(mat, m, n):
             return False
     return True
 
+
 def get_mask_2d_greedy(mat, m, n):
+    """
+    Compute 2D n:m sparse pattern mask for the input matrix mat in greedy way.
+
+    Args:
+        mat (nparray): The input matrix.
+        m (int): m of n:m sparse pattern.
+        n (int): n of n:m sparse pattern.
+    Returns:
+        nparray: The 2D n:m sparse mask of mat.
+    """
     mat_padded, shape = reshape_2d(mat, m)
     mask_padded = np.zeros_like(mat_padded).reshape(-1, m, m)
 
@@ -109,13 +235,15 @@ def get_mask_2d_greedy(mat, m, n):
         sub_mask = np.squeeze(mask_padded[idx])
 
         min_order_1d_indices = np.argsort(sub_mat)
-        min_order_2d_indices = [(int(x/m), x % m) for x in min_order_1d_indices]
+        min_order_2d_indices = [(int(x / m), x % m)
+                                for x in min_order_1d_indices]
         row_counter = collections.Counter()
         col_counter = collections.Counter()
 
         for i in range(len(min_order_1d_indices) - 1, -1, -1):
             matrix_entry = min_order_2d_indices[i]
-            if (row_counter[matrix_entry[0]] == n) or (col_counter[matrix_entry[1]] == n):
+            if (row_counter[matrix_entry[0]] == n) or \
+               (col_counter[matrix_entry[1]] == n):
                 continue
 
             sub_mask[matrix_entry[0], matrix_entry[1]] = 1.0
@@ -132,8 +260,18 @@ def get_mask_2d_greedy(mat, m, n):
             curr_idx += 1
     return mask[:mat.shape[0], :mat.shape[1]]
 
+
 valid_2d_patterns = {}
-def compute_valid_2d_patterns(m,n):
+def compute_valid_2d_patterns(m, n):
+    """
+    Compute all vaild 2D n:m sparse pattern.
+
+    Args:
+        m (int): m of n:m sparse pattern.
+        n (int): n of n:m sparse pattern.
+    Returns:
+        map: A map from m_n (string) to vaild 2D n:m sparse patterns.
+    """
     global valid_2d_patterns
 
     valid_key = '{}_{}'.format(m, n)
@@ -152,12 +290,25 @@ def compute_valid_2d_patterns(m,n):
         valid_2d_patterns[valid_key] = valid_patterns
         return valid_patterns
 
+
 def get_mask_2d_best(mat, m, n):
+    """
+    Compute 2D n:m sparse pattern mask for the input matrix mat in best way.
+
+    Args:
+        mat (nparray): The input matrix.
+        m (int): m of n:m sparse pattern.
+        n (int): n of n:m sparse pattern.
+    Returns:
+        nparray: The 2D n:m sparse mask of mat.
+    """
     patterns = compute_valid_2d_patterns(m, n)
 
     mat_flattern, shape = reshape_2d(mat, m)
     mask_flattern = np.ones_like(mat_flattern).reshape(-1, m, m)
-    pmax = np.argmax(np.matmul(mat_flattern, patterns.reshape(patterns.shape[0],m*m).T), axis=1)
+    pmax = np.argmax(
+        np.matmul(mat_flattern, patterns.reshape(patterns.shape[0], m * m).T),
+        axis=1)
 
     mask_flattern[:] = patterns[pmax[:]]
     mask = np.empty(shape)
@@ -171,7 +322,20 @@ def get_mask_2d_best(mat, m, n):
             curr_idx += 1
     return mask[:mat.shape[0], :mat.shape[1]]
 
+
 def create_mask(tensor, func_name="get_mask_1d_greedy", m=4, n=2):
+    """
+    Create n:m sparse pattern mask of input tensor via function given by func_name.
+    Currently only support tensor with dimension less than or equal to 4.
+
+    Args:
+        tensor (nparray): The input matrix.
+        func_name (string): The function name to generate spase mask.
+        m (int): m of n:m sparse pattern.
+        n (int): n of n:m sparse pattern.
+    Returns:
+        nparray: The n:m sparse mask of mat generated by func_name.
+    """
     shape = tensor.shape
     ttype = tensor.dtype
     t = tensor.astype(float)
@@ -186,16 +350,32 @@ def create_mask(tensor, func_name="get_mask_1d_greedy", m=4, n=2):
         mask = func(t, m, n)
         return mask.reshape(shape).astype(ttype)
     elif len(shape) == 3:
-        t = t.reshape(shape[0]*shape[1], shape[2])
+        t = t.reshape(shape[0] * shape[1], shape[2])
         mask = func(t, m, n)
         return mask.reshape(shape).astype(ttype)
     # 4d-tensor conv (out, in, h, w) -> (out, in*h*w) in GemmConvKernel Op
     elif len(shape) == 4:
-        t = t.reshape(shape[0], shape[1]*shape[2]*shape[3])
+        t = t.reshape(shape[0], shape[1] * shape[2] * shape[3])
         mask = func(t, m, n)
         return mask.reshape(shape).astype(ttype)
+    else:
+        assert True, "The dimension of input tensor is not supported, " \
+                     "Only dimension < 4 is supported but got {}".format(len(shape))
 
-def check_sparsity(tensor, func_name="check_mask_2d", m=4, n=2):
+
+def check_sparsity(tensor, func_name="check_mask_1d", m=4, n=2):
+    """
+    Check if input tensor is in  n:m sparse pattern via function given by func_name.
+    Currently only support tensor with dimension less than or equal to 4.
+
+    Args:
+        tensor (nparray): The input matrix.
+        func_name (string): The function name to check spase mask.
+        m (int): m of n:m sparse pattern.
+        n (int): n of n:m sparse pattern.
+    Returns:
+        bool: True if tensor pass checking via func_name, otherwise False.
+    """
     shape = tensor.shape
     t = tensor.astype(float)
 
@@ -207,82 +387,11 @@ def check_sparsity(tensor, func_name="check_mask_2d", m=4, n=2):
         t = t.reshape(shape[0], shape[1])
         return func(t, m, n)
     elif len(shape) == 3:
-        t = t.reshape(shape[0]*shape[1], shape[2])
+        t = t.reshape(shape[0] * shape[1], shape[2])
         return func(t, m, n)
     # 4d-tensor conv (out, in, h, w) -> (out, in*h*w) in GemmConvKernel Op
     elif len(shape) == 4:
-        t = t.reshape(shape[0], shape[1]*shape[2]*shape[3])
+        t = t.reshape(shape[0], shape[1] * shape[2] * shape[3])
         return func(t, m, n)
 
     return False
-
-if __name__ == "__main__":
-
-    x = np.array([3.00, 6.00, 7.00, 5.00, 3.00, 5.00, 6.00, 2.00, 9.00, 1.00, 2.00, 7.00, 0.00, 9.00, 3.00, 6.00, 0.00, 6.00, 2.00, 6.00, 1.00, 8.00, 7.00, 9.00, 2.00, 0.00, 1.00, 3.00, 7.00, 5.00, 9.00, 2.00, 2.00, 8.00, 9.00, 7.00, 3.00, 6.00, 1.00, 2.00, 9.00, 3.00, 1.00, 9.00, 4.00, 7.00, 8.00, 4.00, 5.00, 0.00, 3.00, 6.00, 1.00, 0.00, 6.00, 3.00, 2.00, 0.00, 6.00, 1.00, 5.00, 3.00, 4.00, 7.00, 6.00, 5.00, 5.00, 9.00, 3.00, 7.00, 4.00, 5.00, 2.00, 5.00, 4.00, 7.00, 4.00, 4.00, 3.00, 0.00, 7.00, 8.00, 6.00, 8.00, 8.00, 4.00, 3.00, 1.00, 4.00, 9.00, 2.00, 0.00, 6.00, 8.00, 9.00, 2.00, 6.00, 3.00, 4.00, 9.00, 5.00, 0.00, 4.00, 8.00, 7.00, 1.00, 7.00, 2.00, 7.00, 2.00, 2.00, 6.00, 1.00, 0.00, 6.00, 0.00, 5.00, 9.00, 4.00, 9.00, 0.00, 9.00, 1.00, 7.00, 7.00, 1.00, 1.00, 5.00, 9.00, 7.00, 5.00, 6.00, 7.00, 3.00, 6.00, 5.00, 6.00, 3.00, 9.00, 4.00, 8.00, 1.00, 2.00, 9.00, 3.00, 9.00, 0.00, 8.00, 8.00, 5.00, 0.00, 9.00, 6.00, 3.00, 8.00, 5.00, 6.00, 1.00, 1.00, 5.00, 9.00, 8.00, 4.00, 7.00, 1.00, 0.00, 3.00, 0.00,4.00, 4.00, 2.00, 1.00, 7.00, 6.00, 3.00, 1.00, 7.00, 5.00, 9.00, 6.00, 2.00, 1.00, 7.00, 8.00, 5.00, 7.00, 4.00, 1.00, 8.00, 5.00, 9.00, 7.00, 5.00, 3.00, 8.00, 8.00, 3.00, 1.00, 8.00, 9.00, 6.00, 4.00, 3.00, 3.00, 3.00, 8.00, 6.00, 0.00, 4.00, 8.00, 8.00, 7.00, 9.00, 7.00, 5.00, 6.00, 4.00, 3.00, 0.00, 2.00, 0.00, 9.00, 2.00, 5.00, 4.00, 0.00, 5.00, 9.00, 4.00, 6.00, 9.00, 2.00, 2.00, 4.00, 7.00, 7.00, 5.00, 4.00, 8.00, 1.00, 2.00, 8.00, 9.00, 3.00, 6.00, 8.00, 0.00, 2.00, 1.00, 0.00, 5.00, 0.00, 1.00, 0.00, 8.00, 5.00])
-    x_ref = np.array([0.00, 6.00, 7.00, 0.00, 0.00, 5.00, 6.00, 0.00, 9.00, 0.00, 0.00, 7.00, 0.00, 9.00, 0.00, 6.00, 0.00, 6.00, 0.00, 6.00, 0.00, 8.00, 0.00, 9.00, 2.00, 0.00, 0.00, 3.00, 7.00, 0.00, 9.00, 0.00, 0.00, 8.00, 9.00, 0.00, 3.00, 6.00, 0.00, 0.00, 9.00, 0.00, 0.00, 9.00, 0.00, 7.00, 8.00, 0.00, 5.00, 0.00, 0.00, 6.00, 0.00, 0.00, 6.00, 3.00, 2.00, 0.00, 6.00, 0.00, 5.00, 0.00, 0.00, 7.00, 6.00, 0.00, 0.00, 9.00, 0.00, 7.00, 0.00, 5.00, 0.00, 5.00, 0.00, 7.00, 4.00, 4.00, 0.00, 0.00, 0.00, 8.00, 0.00, 8.00, 8.00, 4.00, 0.00, 0.00, 4.00, 9.00, 0.00, 0.00, 0.00, 8.00, 9.00, 0.00, 6.00, 0.00, 0.00, 9.00, 5.00, 0.00, 0.00, 8.00, 7.00, 0.00, 7.00, 0.00, 7.00, 0.00, 0.00, 6.00, 1.00, 0.00, 6.00, 0.00, 0.00, 9.00, 0.00, 9.00, 0.00, 9.00, 0.00, 7.00, 7.00, 0.00, 0.00, 5.00, 9.00, 7.00, 0.00, 0.00, 7.00, 0.00, 6.00, 0.00, 6.00, 0.00, 9.00, 0.00, 8.00, 0.00, 0.00, 9.00, 0.00, 9.00, 0.00, 8.00, 8.00, 0.00, 0.00, 9.00, 6.00, 0.00, 8.00, 0.00, 6.00, 0.00, 0.00, 5.00, 9.00, 8.00, 0.00, 0.00, 1.00, 0.00, 3.00, 0.00, 4.00, 4.00, 0.00, 0.00, 7.00, 6.00, 0.00, 0.00, 7.00, 0.00, 9.00, 0.00, 0.00, 0.00, 7.00, 8.00, 5.00, 7.00, 0.00, 0.00, 8.00, 0.00, 9.00, 0.00, 0.00, 0.00, 8.00, 8.00, 0.00, 0.00, 8.00, 9.00, 6.00, 4.00, 0.00, 0.00, 0.00, 8.00, 6.00, 0.00, 0.00, 8.00, 8.00, 0.00, 9.00, 7.00, 0.00, 0.00, 4.00, 3.00, 0.00, 0.00, 0.00, 9.00, 0.00, 5.00, 0.00, 0.00, 5.00, 9.00, 0.00, 6.00, 9.00, 0.00, 0.00, 0.00, 7.00, 7.00, 5.00, 0.00, 8.00, 0.00, 0.00, 8.00, 9.00, 0.00, 6.00, 8.00, 0.00, 0.00, 1.00, 0.00, 5.00, 0.00, 0.00, 0.00, 8.00, 5.00])
-    x_2d = x.reshape((32, 8))
-
-    print("Density of X: ", "density: {:.2f}".format(density(x)*100))
-    print("Density of X_ref: ", "density: {:.2f}".format(density(x_ref)*100))
-
-    a = np.arange(9).reshape(3, 3)
-    print(check_mask_2d(a, 4, 2))
-    print(get_mask_2d_greedy(a, 4, 2))
-
-    mask = get_mask_1d_greedy(x_2d, 4, 2)
-    x_pruned = np.multiply(x_2d, mask)
-    print("Checking non_pruned X_1D:", check_mask_1d(x_2d, 4, 2))
-    print("Checking pruned X_1D:", check_mask_1d(x_pruned, 4, 2))
-
-    mask = get_mask_1d_best(x_2d, 4, 2)
-    x_pruned = np.multiply(x_2d, mask)
-    print("Checking non_pruned X_1D:", check_mask_1d(x_2d, 4, 2))
-    print("Checking pruned X_1D (BEST):", check_mask_1d(x_pruned, 4, 2))
-
-    x_not_in_4 = np.random.randint(5, size=(11, 11))
-    mask = get_mask_1d_greedy(x_not_in_4, 4, 2)
-    x_pruned = np.multiply(x_not_in_4, mask)
-    print("Checking non_pruned X_1D:", check_mask_1d(x_not_in_4, 4, 2))
-    print("Checking pruned X_1D:", check_mask_1d(x_pruned, 4, 2))
-
-    mask_2d = get_mask_2d_greedy(x_2d, 4, 2)
-    x_2d_pruned = np.multiply(x_2d, mask_2d)
-    check_mask_2d(x_2d_pruned, 4, 2)
-    print("Checking non_pruned X_2D:", check_mask_2d(x_2d.transpose(), 4, 2))
-    print("Checking pruned X_2D:", check_mask_2d(x_2d_pruned, 4, 2))
-
-    mask_2d = get_mask_2d_best(x_2d, 4, 2)
-    x_2d_pruned = np.multiply(x_2d, mask_2d)
-    check_mask_2d(x_2d_pruned, 4, 2)
-    print("Checking non_pruned X_2D:", check_mask_2d(x_2d.transpose(), 4, 2))
-    print("Checking pruned X_2D (BEST):", check_mask_2d(x_2d_pruned, 4, 2))
-
-    created_mask_1d = create_mask(x, func_name="get_mask_1d_greedy")
-    created_mask_2d = create_mask(x_2d, func_name="get_mask_2d_greedy")
-    print("Checking created_mask 1D:", check_mask_1d(created_mask_1d, 4, 2))
-    print("Checking created_mask 2D:", check_mask_2d(created_mask_2d, 4, 2))
-
-    pass_verification = True
-    for i in range(30):
-        x_2d = np.random.rand(16, 16)
-        mask_greedy = get_mask_1d_greedy(x_2d, 4, 2)
-        mask_best = get_mask_1d_best(x_2d, 4, 2)
-        if (mask_best == mask_greedy).all() == False:
-            print(i, "Mask1D greedy != best")
-            pass_verification = False
-
-        mask2d_greedy = get_mask_2d_greedy(x_2d, 4, 2)
-        mask2d_best = get_mask_2d_best(x_2d, 4, 2)
-        x_pruned_greedy = np.multiply(x_2d, mask2d_greedy)
-        x_pruned_best = np.multiply(x_2d, mask2d_best)
-        if (x_pruned_best.sum() < x_pruned_greedy.sum()):
-            print(i, "Mask2D greedy > best")
-            pass_verification = False
-
-        if not check_mask_1d(mask_best, 4, 2):
-            print(i, "1D best donot pass checking!")
-        if not check_mask_2d(mask2d_best, 4, 2):
-            print(i, "2D best donot pass checking!")
-    print("Best masking checking:", pass_verification)
-
-
